0797a68c1a3ebc4fb3ad49bc4565ededd5c0e802
Yovie Alfa Guistuta <cobacobas800@gmail.com>
2024-09-25 13:46:20 +0700

    -

--- src/pages/DataTable.tsx
+++ src/pages/DataTable.tsx
@@ -1,23 +1,4 @@
-interface DataUser {
-    nama: string;
-    umur: number;
-    alamat: string
-}
-
 const DataTable = () => {
-    const dataUser = [
-        {
-            "nama": "Andi",
-            "umur": 20,
-            "alamat": "Pasaraya Blok M Gedung B Lt. 6 Jalan Iskandarsyah II No.7, RW. 2, Melawai, Kebayoran Baru, RT.3/RW.1, Melawai, Kby. Baru Kota Jakarta Selatan"
-        },
-        {
-            "nama": "Nurul",
-            "umur": 25,
-            "alamat": "Balikpapan. Jalan MT Haryono RT 011/005 Kelurahan Damai, Kecamatan Balikpapan Selatan"
-        }
-    ]
-    
     return (
         <>
             <table>
